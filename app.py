e4f6a1f3f05132ef173bd667f8e82b8314dbffec
RohitKM25 <rohitkmanoj@outlook.com>
2022-11-22 22:46:20 +0530

    Fixed find flights command and made optimizations.

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import os
 import csv
 import random
 import datetime
-from utils import *
+import colorama as cm
+from tabulate import tabulate
+
 
 cm.init(convert=True)
 # prerequisites
@@ -25,6 +27,114 @@ LOCAL_STORAGE = None
 SESSION_STORAGE = {
     'current_user': ''
 }
+
+# colors for colored outputs
+FORE_COLORS = {
+    'i': cm.Fore.LIGHTWHITE_EX,
+    'a': cm.Fore.LIGHTCYAN_EX,
+    'a2': cm.Fore.CYAN,
+    'w': cm.Fore.YELLOW,
+    'e': cm.Fore.RED,
+    's': cm.Fore.GREEN,
+    'd': cm.Fore.LIGHTWHITE_EX,
+    'r': cm.Fore.RESET,
+}
+
+
+def get_random_letter(): return chr(random.randint(65, 90))
+
+
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def get_random_date():
+    year = datetime.datetime.now().date().year
+    month = random.randint(1, 12)
+    day = months[month-1]
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 5)*10
+    return datetime.datetime(year, month, day, hour, minute)
+
+
+def colored_str(values: str, type='d', data=None):
+    prt = ''
+    if not data:
+        prt = FORE_COLORS[type] + values + FORE_COLORS['r']
+    else:
+        out = values.split('{}')
+        for i in out:
+            prt += FORE_COLORS[type] + i + FORE_COLORS['r']
+            if len(data) > 0:
+                val = data.pop(0)
+                prt += FORE_COLORS[val[0]] + val[1] + FORE_COLORS['r']
+    return prt
+
+
+def print_colored(values: str, type='d', data=None, end='\n'):
+    '''
+    Colors outputs by type.
+    '''
+    print(colored_str(values, type=type, data=data), end=end)
+
+
+def tabed(iterable):
+    print(tabulate(iterable, tablefmt="fancy_grid", headers='keys'))
+
+
+def join(l, sep=''):
+    str = ''
+    for i in l:
+        str += f'{i}{sep}'
+    return str if sep == '' else str[:-1]
+
+
+def input_colored(values: str, type='d', default=None, data=None):
+    print_colored((values+'(Default="{}") ' if default else values), type=type, data=(
+        [['a', default]] if default and not data else (data+[['a', default]] if default and data else data)), end='')
+    print(cm.Fore.WHITE, end='')
+    inp = input('')
+    print(FORE_COLORS['r'], end='')
+    return default if inp == '' and default else inp
+
+
+def str_to_datetime(str):
+    try:
+        str = datetime.datetime.strptime(str, '%Y-%m-%d %H:%M')
+    except ValueError:
+        try:
+            str = datetime.datetime.strptime(
+                str, '%Y-%m-%d')
+        except ValueError:
+            print_colored(
+                'Please enter date as yyyy-mm-dd or yyyy-mm-dd hh:mm', type='e')
+            return None
+    return str
+
+
+def input_colored_type_casted(values: str, val_type: str, type='d', data=None):
+    print_colored(values, type=type, data=data, end='')
+    print(cm.Fore.WHITE, end='')
+    inp = input()
+    print(FORE_COLORS['r'], end='')
+    val_type = str(val_type)
+    if val_type.lower().find('varchar') != -1:
+        if '(' in val_type.lower():
+            len = int(val_type.split('(')[1][:-2])
+            return inp[:len-1]
+        else:
+            return inp[:254]
+    elif val_type.lower().find('int') != -1:
+        return int(inp)
+    elif val_type.lower().find('float') != -1:
+        return float(inp)
+    elif val_type.lower().find('datetime') != -1:
+        return inp
+    else:
+        return inp
+
+
+def get_list_from_1col_dict(l):
+    return [i[list(l[0].keys())[0]] for i in l] if len(l) > 0 else []
 
 
 def check_database_exists():
@@ -82,36 +192,6 @@ def new_user(values):
         return False
 
 
-def initialize_database():
-    with open(f"{os.path.dirname(__file__)}\\dbinit.sql", "r") as db_file:
-        script = db_file.read()
-        for i in script.split(';'):
-            try:
-                if i.strip() != '':
-                    mscur.execute(i)
-            except mysql.connector.Error as err:
-                print_colored(f'ERROR: {err.msg}', type='e')
-
-    files = ['airport', 'airliner']
-    for i in files:
-        with open(f"{os.path.dirname(__file__)}\\init_data\\{i}.dbinit.csv", "r", newline='', encoding='utf8') as init_file:
-            content = list(csv.reader(init_file))
-            content = [{content[0][j]:content[i][j] for j in range(
-                len(content[0]))} for i in range(1, len(content))]
-            try:
-                mscur.execute(f'desc {i}')
-                cols = mscur.fetchall()
-                fields = [i['Field'] for i in cols]
-                for k in range(len(content)):
-                    if 'id' in fields:
-                        content[k]['id'] = k+1
-                    new_record(i, content[k])
-            except mysql.connector.Error as err:
-                print_colored(f'ERROR: {err.msg}', type='e')
-            print_colored('Loaded data into "{}".', type='s', data=[['a', i]])
-    print_colored('Successfully loaded into database.', type='s')
-
-
 def app_settings_set(key, value):
     settings = None
     with open(f'{os.path.expanduser("~")}\\flight-ticket-booking-settings.json', 'r') as settings_file:
@@ -155,6 +235,11 @@ def get_flight_data_from_fare_id(fare_id):
         f'select flight.id "flight_id",flight.departure_on "flight_departure_on",dep.name "departure_airport_name",arr.name "arrival_airport_name",fare.cancellation_fee "fare_cancellation_fee", fare.tag "fare_tag" from airport dep,airport arr,flight,fare where fare.id = {fare_id} and flight.id = fare.flight_id and dep.code = flight.departure_airport_code and arr.code = flight.arrival_airport_code', multi=False)
     return mscur.fetchone()
 # app commands
+
+
+def print_title(title):
+    print_colored('\n'+title.upper(), type='a2')
+    print_colored('-'*len(title), end='')
 
 
 def sign_in():
@@ -202,6 +287,42 @@ def view_settings():
         settings = json.load(settings_file)
         tabed([{'Category': i.split(':')[0], 'Setting':i.split(":")
               [1], 'Current Value':settings[i]} for i in settings])
+
+
+def initialize_database(ask=True):
+
+    if check_database_exists():
+        if ask:
+            if input_colored('Do you you want to delete the database and run initialization.', default='y').lower != 'y':
+                print_colored('Cancelled inittialization.')
+
+    with open(f"{os.path.dirname(__file__)}\\dbinit.sql", "r") as db_file:
+        script = db_file.read()
+        for i in script.split(';'):
+            try:
+                if i.strip() != '':
+                    mscur.execute(i)
+            except mysql.connector.Error as err:
+                print_colored(f'ERROR: {err.msg}', type='e')
+
+    files = ['airport', 'airliner']
+    for i in files:
+        with open(f"{os.path.dirname(__file__)}\\init_data\\{i}.dbinit.csv", "r", newline='', encoding='utf8') as init_file:
+            content = list(csv.reader(init_file))
+            content = [{content[0][j]:content[i][j] for j in range(
+                len(content[0]))} for i in range(1, len(content))]
+            try:
+                mscur.execute(f'desc {i}')
+                cols = mscur.fetchall()
+                fields = [i['Field'] for i in cols]
+                for k in range(len(content)):
+                    if 'id' in fields:
+                        content[k]['id'] = k+1
+                    new_record(i, content[k])
+            except mysql.connector.Error as err:
+                print_colored(f'ERROR: {err.msg}', type='e')
+            print_colored('Loaded data into "{}".', type='s', data=[['a', i]])
+    print_colored('Successfully loaded into database.', type='s')
 
 
 def admin_add():
@@ -312,46 +433,66 @@ def find_flights():
 
     airports = get_table_data('airport')
 
-    departure_airport_code = None
-    arrival_airport_code = None
-
-    def check_airport(loc, airport):
-        return loc.lower() in airport['city'].lower() or loc.lower() in airport['code'].lower() or loc.lower() in airport['region'].lower() or loc.lower() in airport['name'].lower()
-
-    for i in airports:
-        if from_location != '' and check_airport(from_location, i):
-            departure_airport_code = i['code']
-        if to_location != '' and check_airport(to_location, i):
-            arrival_airport_code = i['code']
-
     now = datetime.datetime.now()
     from_date = input_colored('From: ', default=now.strftime('%Y-%m-%d'))
     to_date = input_colored('To: ', default=now.replace(
         month=now.month+1).strftime('%Y-%m-%d'))
-    query_departure_airport_code = f'departure_airport_code = "{departure_airport_code}" and ' if departure_airport_code else ''
-    query_arrival_airport_code = f'arrival_airport_code = "{arrival_airport_code}" and ' if arrival_airport_code else ''
-    query = f'select * from flight where {query_departure_airport_code}{query_arrival_airport_code}departure_on between "{"2022-01-01" if from_date=="-" else from_date}" and "{"2022-12-31" if to_date=="-" else to_date}"'
+    # query = f'select * from flight where {query_departure_airport_code}{query_arrival_airport_code}departure_on between "{"2022-01-01" if from_date=="-" else from_date}" and "{"2022-12-31" if to_date=="-" else to_date}"'
 
-    mscur.execute(query)
-    flights = mscur.fetchall()
+    flights = get_table_data('flight')
+
     for i in flights:
         for j in airports:
             if 'departure_airport_code' in i and j['code'] == i['departure_airport_code']:
                 del i['departure_airport_code']
                 i['departure_airport'] = j['name']
-                i['departure_location'] = f"{j['city']}, {j['region']}"
+                i['departure_location'] = f"{j['name']}, {j['city']}, {j['region']}"
             if 'arrival_airport_code' in i and j['code'] == i['arrival_airport_code']:
                 del i['arrival_airport_code']
                 i['arrival_airport'] = j['name']
-                i['arrival_location'] = f"{j['city']}, {j['region']}"
+                i['arrival_location'] = f"{j['name']}, {j['city']}, {j['region']}"
+
+    flights_filtered = []
+
+    if from_date != '-':
+        from_date = str_to_datetime(from_date)
+        if from_date == None:
+            return
+    if to_date != '-':
+        to_date = str_to_datetime(to_date)
+        if to_date == None:
+            return
+
+    filters_print_data = [['a', from_location if from_location != '' else 'All'], [
+        'a', to_location if to_location != '' else 'All']]
+    if from_date != '-':
+        filters_print_data.append([
+            'a', from_date.strftime('%Y-%m-%d %H:%M')])
+    if to_date != '-':
+        filters_print_data.append(['a', to_date.strftime('%Y-%m-%d %H:%M')])
+
+    departure_date_filter_print = f"\nDeparture Date:{' From {}' if from_date != '-' else ''}{' Till {}' if to_date != '-' else ''}" if from_date != '-' or to_date != '-' else ''
+    print_title('filters applied')
+    print_colored(f'''
+Departure: {{}}
+Arrival: {{}}{departure_date_filter_print}''', data=filters_print_data)
+
     for i in flights:
+        if (i['departure_location'].lower().find(from_location.lower()) != -1 if from_location != '' else True) and (i['arrival_location'].lower().find(to_location.lower()) != -1 if to_location != '' else True) and (i['departure_on'] > from_date if from_date != '-' else True) and (i['departure_on'] < to_date if to_date != '-' else True):
+            flights_filtered.append(i)
+
+    if len(flights_filtered) == 0:
+        print('\nNo flights matching the filters are available.', type='e')
+        return
+    print_title('available flights')
+    for i in flights_filtered:
         content = f'''
 Flight {{}}, {get_airliner_from_airliner_code(i["airliner_code"])}
-From {{}},{i["departure_airport"]} on {{}}
-To {{}},{i["arrival_airport"]}
+From {{}} on {{}}
+To {{}}
 Duration {str(datetime.timedelta(hours=i['duration'])).split(':')[0]}h{str(datetime.timedelta(hours=i['duration'])).split(':')[1]}m.'''
         print_colored(content, data=[['a', i["id"]], ['a', i["departure_location"]], [
-                      'a2', i["departure_on"].strftime('%Y-%m-%d %H:%M:%S')], ['a', i["arrival_location"]]])
+                      'a2', i["departure_on"].strftime('%Y-%m-%d %H:%M')], ['a', i["arrival_location"]]])
 
 
 def get_fares():
@@ -364,13 +505,14 @@ def get_fares():
     fares = mscur.fetchall()
 
     if len(fares) == 0:
-        print_colored(
-            'Fares for {} are unavailable. Or the flight id entered may be incorrect.')
+        print('Fares for {} are unavailable. Or the flight id entered may be incorrect.', type='e', data=[
+              ['a', flight_id]])
+        return
+    print_title(colored_str('Fares for flight {}', data=[['a', flight_id]]))
 
     min_amount = min([i['amount'] for i in fares])
     max_amount = max([i['amount'] for i in fares])
 
-    print_colored('Fares for flight {}:', data=[['a', flight_id]])
     for i in fares:
         if i['amount'] == min_amount:
             print('\n'+tabulate([[f'''
@@ -433,12 +575,15 @@ def my_bookings():
 
     user = get_user_by_email(SESSION_STORAGE['current_user'])
 
-    print_colored('{}\'s bookings', data=[
-                  ['a', user['name']]])
-
     mscur.execute(
         f'select * from booking where user_email = "{user["email"]}" and is_cancelled = false order by booked_on desc')
     bookings = mscur.fetchall()
+
+    if len(bookings) == 0:
+        print('{} has no boookings.', type='e', data=[
+              ['a', user['name']]])
+        return
+    print_title(colored_str('Bookings for {}', data=[['a', user['name']]]))
 
     for i in range(len(bookings)):
         airliner_name = get_airliner_name_from_fare_id(bookings[i]["fare_id"])
@@ -450,7 +595,7 @@ From {} to {}
 On {}.
 Cancellation Fee: {}'''
         print_colored(content, data=[['a2', str(i)], ['a',  flight_data['fare_tag'].title()], ['a', airliner_name], ['a', flight_data['flight_id']], ['a', flight_data['departure_airport_name']], [
-                      'a', flight_data['arrival_airport_name']], ['a', flight_data['flight_departure_on'].strftime('%Y-%m-%d %H:%M:%S')], ['e', str(flight_data['fare_cancellation_fee'])]])
+                      'a', flight_data['arrival_airport_name']], ['a', flight_data['flight_departure_on'].strftime('%Y-%m-%d %H:%M')], ['e', str(flight_data['fare_cancellation_fee'])]])
     return bookings
 
 
@@ -475,7 +620,14 @@ def cancel_booking():
         print_colored(f'ERROR: {err.msg}', type='e')
 
 
-# dictionary of all commands
+def show_help():
+    print_colored('Commands:')
+    for i in commands:
+        print_colored('\t{} => {}', data=[
+                      ['a2', i], ['d', commands[i]['desc']]])
+
+
+# dictionary of all commandsPakistan
 commands = {
     'signin': {'cmd': sign_in, 'desc': 'Signs a user, using email and password. User stored locally.'},
     'signup': {'cmd': sign_up, 'desc': 'Adds the user to database.'},
@@ -489,8 +641,8 @@ commands = {
     'admin add random flights': {'cmd': admin_add_random_flight_repeat, 'desc': 'Adds a new flights with random data.'},
     'admin add random fares': {'cmd': admin_add_random_fare_repeat, 'desc': 'Adds a new fares with random data.'},
     'admin view': {'cmd': admin_view, 'desc': 'Displays records of given table.'},
-
-
+    'init database': {'cmd': initialize_database, 'desc': 'Initializes the database.'},
+    'show help': {'cmd': show_help, 'desc': 'Shows all the commands and brief descriptions.'}
 }
 
 # main
@@ -538,20 +690,17 @@ mscur = mysqlcnn.cursor(dictionary=True)
 
 # checking database
 if not check_database_exists():
-    #     print_colored('Database "{}" exists.', type='s',
-    #                   data=[['a', 'flight_ticket_booking']])
-    # else:
     print_colored(
         'Database does not exist.', type='e')
     print_colored(
         'Initializing database.', type='i')
-    initialize_database()
+    initialize_database(ask=False)
 
 
 print_colored('FLIGHT TICKET BOOKING', type='a')
 print_colored('---------------------')
 print_colored('By {}, {}, {}.', data=[['a2', 'ROHIT K MANOJ'], [
-              'a2', 'DARSHAN M'], ['a2', 'AADI DEV S']])
+              'a2', 'M DARSHAN'], ['a2', 'AADI DEV S']])
 print()
 
 # checking user signed in.
@@ -562,13 +711,12 @@ if LOCAL_STORAGE['app:signed_in_user'] != '':
         print_colored('Hi {}!', data=[['a', user['name']]])
     else:
         print_colored('{} signed in but not recognised. Please sign in again to rectify issue.', data=[
-                      ['a', SESSION_STORAGE['current_user']]], type='w')
+                      ['a', LOCAL_STORAGE['app:signed_in_user']]], type='w')
 else:
     print_colored('No user signed in. use "{}"',
                   data=[['a', 'signin']], type='w')
-print_colored('Commands:')
-for i in commands:
-    print_colored('\t{} => {}', data=[['a2', i], ['d', commands[i]['desc']]])
+
+show_help()
 
 # command loop
 while True:
